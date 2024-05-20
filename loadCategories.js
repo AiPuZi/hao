cedf69701c52a1e2b10b5659daefa0d997fd6fb1
AiPuZi <162868913+AiPuZi@users.noreply.github.com>
2024-05-20 21:37:21 +0800

    更新应用程序静态文件

--- loadCategories.js
+++ loadCategories.js
@@ -1,20 +1,16 @@
 // 确保文档加载完毕后再绑定事件监听器
 document.addEventListener('DOMContentLoaded', function() {
     // 绑定点击事件到导航栏，使用事件委托
-    document.querySelector('.poetry-navigation').addEventListener('click', function(event) {
+    document.querySelector('.navigation').addEventListener('click', function(event) {
         event.preventDefault(); // 阻止默认的链接跳转
 
         var target = event.target; // 获取被点击的元素
         if (target.tagName === 'A') { // 确认点击的是链接
             var category = target.getAttribute('href').substring(1); // 获取分类标识符，移除'#'
 
-            if (category !== 'hanzi') {
-                // 非汉字分类，加载对应分类数据
-                loadCategoryData(category);
-            } else {
-                // 汉字分类，调用显示汉字内容的函数
-                renderCharacters(0); // 假设你的加载汉字内容的函数名为 renderCharacters
-            }
+            // 示例代码中没有提供`renderCharacters`函数，这里假设它是用于渲染汉字内容的函数
+            // 如果你有不同的分类处理逻辑，可以在这里调整
+            loadCategoryData(category);
         }
     });
 });
